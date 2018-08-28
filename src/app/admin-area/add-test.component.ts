1942e22a1bffaf30ea8c2165f62f6caaf1ccc721
dev2arch <sisaudiya.dewan17@gmail.com>
2018-08-28 19:22:29 +0530

    add questions

--- src/app/admin-area/add-test.component.ts
+++ src/app/admin-area/add-test.component.ts
@@ -12,6 +12,8 @@ export class AddTestComponent implements OnInit {
   detaileSubmitted;
   formLength = 0;
   response;
+  testCode;
+  quesRes;
   constructor(private testService:  AllTestService) { }
 
   ngOnInit() {
@@ -19,7 +21,7 @@ export class AddTestComponent implements OnInit {
   }
 
   public addTestDetails(testDetails: NgForm) {
-    alert("hii")
+    this.testCode = testDetails.value.testCode;
     console.log(testDetails.value)
     var today = new Date();
     var dd = today.getDate();
@@ -62,12 +64,58 @@ export class AddTestComponent implements OnInit {
       testDurationInMinutes: testDetails.value.testDuration,
       type: testDetails.value.testType
     }
+
     console.log(testDetailsObj)
     this.testService.addTestDetails(testDetailsObj).subscribe(
       (res) => this.response = res,
       (err) => console.log(err)
     )
     this.detaileSubmitted = true
+
+  }
+   fileChangetemp(event) {
+     this.testCode = 'AIPMT005'
+    this.testService.addQuestionstemp(event, this.testCode)
+      .subscribe(
+        (res) => this.quesRes = res,
+        (err) => console.log(err)
+      )
+  }
+  fileChange(text) {
+    text = JSON.parse(text)
+    this.testCode = 'AIPMT005'
+    alert("caleed")
+     console.log(text)
+    // let fileList: FileList = event.target.files;
+    // console.log(fileList[0])
+    this.testService.addQuestions(text, this.testCode)
+      .subscribe(
+        (res) => this.quesRes = res,
+        (err) => console.log(err)
+      )
+  }
+  openFile(event) {
+    var text;
+    let input = event.target;
+    for (var index = 0; index < input.files.length; index++) {
+      let reader = new FileReader();
+      (reader.readAsText(input.files[index]));
+      reader.onload = () => {
+        // this 'text' is the content of the file
+        text = reader.result;
+        console.log(JSON.parse(text))
+        this.fileChange(text)
+      }
+
+
+    }
+  }
+  public addTestQuestions(testQuestions: NgForm) {
+    this.testCode = 'AIPMT005'
+    this.testService.addQuestions(testQuestions, this.testCode).subscribe(
+      (res) => this.quesRes = res,
+      (err) => console.log(err)
+    )
   }
 
 }
--- src/app/services/all-test.service.ts
+++ src/app/services/all-test.service.ts
@@ -1,7 +1,8 @@
 import 'rxjs/Rx';
 import { Injectable } from '@angular/core';
-import {Http, Response, Headers} from "@angular/http";
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
 import {AppConfig} from '../app.config';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class AllTestService {
@@ -45,6 +46,37 @@ export class AllTestService {
 
     var url = AppConfig.API_ENDPOINT + '/quizzard/test'
     var dataToPost = testDetails;
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization', AppConfig.CURR_USER.token);
+    return this.http.post(url, (dataToPost), {headers: headers})
+      .map(
+        (response: Response) => { this.res = response; console.log("success", response)},
+        error => console.log(error, "error in adding Test")
+      )
+  }
+  public addQuestionstemp(event, testCode) {
+    var url = AppConfig.API_ENDPOINT + '/quizzard/test/' + testCode
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+      let formData: FormData = new FormData();
+      formData.append('file', file);
+      let headers = new Headers();
+      /** In Angular 5, including the header Content-Type can invalidate your request */
+       headers.append('Content-Type', 'multipart/form-data');
+      headers.append('Authorization', AppConfig.CURR_USER.token);
+      // let options = new RequestOptions({ headers: headers });
+      return this.http.post(url, formData, {headers})
+        .map(res => res.json())
+        .catch(error => Observable.throw(error))
+    }
+  }
+  public addQuestions(data, testCode) {
+
+
+    var url = AppConfig.API_ENDPOINT + '/quizzard/test/' + testCode
+    var dataToPost = data;
     var headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Authorization', AppConfig.CURR_USER.token);
