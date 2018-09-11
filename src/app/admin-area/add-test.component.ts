ed457a5d69ab3585b72cdfc298bced63fd59df14
dev2arch <sisaudiya.dewan17@gmail.com>
2018-09-11 23:50:28 +0530

    question jason added

--- src/app/admin-area/add-test.component.ts
+++ src/app/admin-area/add-test.component.ts
@@ -77,7 +77,7 @@ export class AddTestComponent implements OnInit {
     this.testService.addQuestionstemp(event, this.testCode)
       .subscribe(
         (res) => this.quesRes = res,
-        (err) => console.log("tserror>>>", err)
+        (err) => console.log("tserror>>", err)
       )
   }
 
